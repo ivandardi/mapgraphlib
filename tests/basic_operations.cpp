8287a616b4da23503ff6c70300793704144f26d3
Ivan Dardi <ivy.dardi@gmail.com>
2017-03-30 02:18:51 -0300

    Update testing to BDD-style

--- tests/basic_operations.cpp
+++ tests/basic_operations.cpp
@@ -1,6 +1,9 @@
 #include "graph.hpp"
+#include <algorithm>
 #include <catch.hpp>
+#include <cstddef>
 #include <iostream>
+#include <numeric>
 #include <unordered_map>
 #include <utility>
 
@@ -18,46 +21,110 @@ std::ostream& operator<<(std::ostream& out, Graph<T, is_directed>& g)
     return out;
 }
 
-TEST_CASE("Basic Operations", "[basic-operations]")
+SCENARIO("Basic Operations", "[basic-operations]")
 {
 
-    SECTION("edge_insertion")
+    GIVEN("An undirected graph with 5 vertices")
     {
+
         UndirectedGraph<int> graph;
         std::unordered_map<int, UndirectedGraph<int>::iterator> vertice_tags;
 
-        REQUIRE(graph.num_vertices() == 0);
-        REQUIRE(graph.num_edges() == 0);
+        const int num_vertices = 5;
 
-        constexpr int inserted = 5;
-        for (int i = 0; i < inserted; ++i) {
+        for (int i = 0; i < num_vertices; ++i) {
             vertice_tags[i] = graph.insert(i);
         }
 
-        REQUIRE(graph.num_vertices() == inserted);
-        REQUIRE(graph.num_edges() == 0);
+        WHEN("The graph is complete")
+        {
 
-        std::cout << graph << '\n';
+            for (int i = 0; i < num_vertices - 1; ++i) {
+                for (int j = i + 1; j < num_vertices; ++j) {
+                    graph.add_edge(vertice_tags[i], vertice_tags[j]);
+                }
+            }
+
+            THEN("The number of edges must be (n*(n-1))/2")
+            {
+                REQUIRE((num_vertices * (num_vertices - 1)) / 2 == graph.num_edges());
+            }
+
+            THEN("All vertices must have degree n-1")
+            {
+                for (auto&& elem : vertice_tags) {
+                    REQUIRE((num_vertices - 1) == graph.degree(elem.second));
+                }
+            }
+        }
+
+        WHEN("The graph is emptied")
+        {
+
+            graph.clear();
+
+            THEN("The number of vertices and edges should be 0")
+            {
+                REQUIRE(0 == graph.num_vertices());
+                REQUIRE(0 == graph.num_edges());
+            }
+        }
     }
 
-    SECTION("edge_insertion")
+    GIVEN("A directed graph with 5 vertices")
     {
-        UndirectedGraph<int> graph;
 
-        REQUIRE(graph.num_vertices() == 0);
-        REQUIRE(graph.num_edges() == 0);
+        DirectedGraph<int> graph;
+        std::unordered_map<int, DirectedGraph<int>::iterator> vertice_tags;
 
-        auto u = graph.insert(10);
-        auto v = graph.insert(20);
+        const int num_vertices = 5;
 
-        REQUIRE(graph.num_vertices() == 2);
-        REQUIRE(graph.num_edges() == 0);
+        for (int i = 0; i < num_vertices; ++i) {
+            vertice_tags[i] = graph.insert(i);
+        }
 
-        graph.add_edge(u, v);
+        WHEN("The graph is complete")
+        {
 
-        REQUIRE(graph.num_vertices() == 2);
-        REQUIRE(graph.num_edges() == 1);
+            for (int i = 0; i < num_vertices - 1; ++i) {
+                for (int j = i + 1; j < num_vertices; ++j) {
+                    graph.add_edge(vertice_tags[i], vertice_tags[j]);
+                }
+            }
 
-        std::cout << graph << '\n';
+            size_t out_degree = 0;
+            size_t in_degree = 0;
+            for (DirectedGraph<int>::iterator it = graph.begin(); it != graph.end(); ++it) {
+                out_degree += graph.out_degree(it);
+                in_degree += graph.in_degree(it);
+            }
+
+            THEN("The sum of indegrees must be equal to the sum of outdegrees")
+            {
+                REQUIRE(in_degree == out_degree);
+            }
+
+            THEN("The sum of the indegrees must be equal to the number of edges")
+            {
+                REQUIRE(in_degree == 0);
+            }
+
+            THEN("The sum of the outdegrees must be equal to the number of edges")
+            {
+                REQUIRE(out_degree == 0);
+            }
+        }
+
+        WHEN("The graph is emptied")
+        {
+
+            graph.clear();
+
+            THEN("The number of vertices and edges should be 0")
+            {
+                REQUIRE(0 == graph.num_vertices());
+                REQUIRE(0 == graph.num_edges());
+            }
+        }
     }
 }
